21590c0c114fc88d09263e50807cc1d36341879f
Your Name <email@example.com>
2023-12-05 12:15:45 +0800

    add

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # 本项目存储一些自己所用的数据结构代码模板
 ## 目录
 ****
+**[个人做题技巧总结 TopicSkills.go](https://github.com/vapausw/MyDataStructureTemplates/blob/test/TopicSkills.go)**
+
 **[栈 stack.go](https://github.com/vapausw/MyDataStructureTemplates/blob/test/stack.go)**
 
 **[队列 queue.go](https://github.com/vapausw/MyDataStructureTemplates/blob/test/queue.go)**
@@ -12,6 +14,7 @@
 **[单调队列 monotone_queue.fo](https://github.com/vapausw/MyDataStructureTemplates/blob/test/monotone_queue.go)**
 
 **[并查集 UnionFind.go](https://github.com/vapausw/MyDataStructureTemplates/blob/test/UnionFind.go)**
+
 ****
 ## 代码参考
 **[go语言代码多参考于灵神的代码模板](https://github.com/EndlessCheng/codeforces-go)**
--- test.go
+++ test.go
@@ -1,38 +1,5 @@
 package main
 
 func main() {
-	roads := [][]int{{0, 1}, {0, 2}, {1, 3}, {1, 4}}
-	minimumFuelCost(roads, 5)
-}
 
-func minimumFuelCost(roads [][]int, seats int) int64 {
-	n := len(roads)
-	g := make([][]int, n+1)
-	for _, e := range roads {
-		x, y := e[0], e[1]
-		g[x] = append(g[x], y)
-		g[y] = append(g[y], x)
-	}
-	ans := 0
-	var dfs func(int, int) (int, int) //返回车辆数以及目前所有的乘客数量
-	dfs = func(x, fa int) (int, int) {
-		flag := true
-		res, cnt := 0, 0
-		for _, y := range g[x] {
-			if y != fa {
-				flag = false
-				res1, cnt1 := dfs(y, x)
-				res += res1
-				cnt += cnt1
-			}
-		}
-		ans += res
-		if flag {
-			return 1, 1
-		}
-		res = (cnt + seats) / seats // 当前乘客数量合并,向上取整
-		return res, cnt + 1
-	}
-	dfs(0, -1)
-	return int64(ans)
 }
