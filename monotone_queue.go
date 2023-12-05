32eb02c693e91eab11f336a634fbfb6229affba0
Your Name <email@example.com>
2023-12-05 15:46:43 +0800

    add

--- monotone_queue.go
+++ monotone_queue.go
@@ -1,5 +1,86 @@
 package main
 
+// 以下是单调队列模板可以直接复制使用
+
+type MonotoneQueue struct {
+	Data []any
+	Size int //单调队列大小
+}
+
+// Less 区间维护逻辑函数
+func (mq *MonotoneQueue) Less(a, b any) bool {
+	return a.(int) > b.(int) // >= 维护区间最大值，单调递增队列， <= 维护区间最小值，单调递减队列， =判断队列中是否允许重复值存在
+}
+
+// PushPurge  清除队列中不合法的元素，写此函数是因为有时需要更新之前计算答案
+func (mq *MonotoneQueue) PushPurge(v any) {
+	for mq.Size > 0 && mq.Less(v, mq.Data[mq.Size-1]) {
+		mq.Data = mq.Data[:mq.Size-1]
+		mq.Size--
+	}
+}
+
+// Push 向单调队列中添加元素
+func (mq *MonotoneQueue) Push(v any) {
+	mq.Size++
+	mq.Data = append(mq.Data, v)
+}
+
+// Pop 删除单调队列的第一个元素并返回
+func (mq *MonotoneQueue) Pop() any {
+	if mq.Size <= 0 {
+		return -1 //非法操作
+	}
+	v := mq.Data[0]
+	mq.Data = mq.Data[1:]
+	mq.Size--
+	return v
+}
+
+// Top 返回单调队列的第一个元素
+func (mq *MonotoneQueue) Top() any {
+	if mq.Size <= 0 {
+		return -1
+	}
+	return mq.Data[0]
+}
+
+// 单调队列模板题目 https://leetcode.cn/problems/dui-lie-de-zui-da-zhi-lcof/
+
+type Checkout struct {
+	values []int
+	q      MonotoneQueue
+}
+
+func Constructor() Checkout {
+	return Checkout{}
+}
+
+func (this *Checkout) Get_max() int {
+	if this.q.Size == 0 {
+		return -1
+	}
+	return this.q.Top().(int)
+}
+
+func (this *Checkout) Add(value int) {
+	this.values = append(this.values, value)
+	this.q.PushPurge(value)
+	this.q.Push(value)
+}
+
+func (this *Checkout) Remove() int {
+	if len(this.values) == 0 {
+		return -1
+	}
+	temp := this.values[0]
+	this.values = this.values[1:]
+	if temp == this.q.Top().(int) {
+		return this.q.Pop().(int)
+	}
+	return temp
+}
+
 // 单调队列题目 https://leetcode.cn/problems/shortest-subarray-with-sum-at-least-k/description/
 
 // 此处先使用前缀和方便计算子数组的值
--- monotone_stack.go
+++ monotone_stack.go
@@ -1,16 +1,75 @@
 package main
 
-// 单调栈模板题 https://leetcode.cn/problems/daily-temperatures/
-// 解题代码如下
+// 以下是单调栈模板可以直接复制使用
+
+type MonotoneStack struct {
+	Data []any
+	Size int //单调栈大小
+}
+
+// Less 区间维护逻辑函数
+func (mq *MonotoneStack) Less(a, b any) bool {
+	return a.(int) < b.(int) // >= 维护区间最大值，单调递增队列， <= 维护区间最小值，单调递减队列， =判断队列中是否允许重复值存在
+}
+
+// PushPurge 清除栈中不合法的元素，写此函数是因为有时需要更新之前计算答案，具体看下述用例
+func (mq *MonotoneStack) PushPurge(v any) {
+	for mq.Size > 0 && mq.Less(v, mq.Data[mq.Size-1]) {
+		mq.Pop()
+	}
+}
+
+func (mq *MonotoneStack) Push(v any) {
+	mq.Size++
+	mq.Data = append(mq.Data, v)
+}
+
+// Pop 删除单调栈的最后一个元素并返回
+func (mq *MonotoneStack) Pop() any {
+	if mq.Size <= 0 {
+		return -1 //非法操作
+	}
+	v := mq.Data[mq.Size-1]
+	mq.Data = mq.Data[:mq.Size-1]
+	mq.Size--
+	return v
+}
+
+// Top 返回单调栈的最后一个元素
+func (mq *MonotoneStack) Top() any {
+	if mq.Size <= 0 {
+		return -1
+	}
+	return mq.Data[mq.Size-1]
+}
+
+// 使用上面单调栈模板的示例 https://leetcode.cn/problems/final-prices-with-a-special-discount-in-a-shop/
+func finalPrices(prices []int) []int {
+	var q MonotoneStack
+	ans := make([]int, len(prices))
+	for i := len(prices) - 1; i >= 0; i-- {
+		q.PushPurge(prices[i])
+		if q.Size == 0 {
+			ans[i] = prices[i]
+		} else {
+			ans[i] = prices[i] - q.Top().(int)
+		}
+		q.Push(prices[i])
+	}
+	return ans
+}
+
+// 手写单调栈模板题 https://leetcode.cn/problems/daily-temperatures/
+// 解题代码如下，因为要在维护的过程中更新答案，而且维护的是下标不是单一的值
 func dailyTemperatures(temperatures []int) []int {
 	n := len(temperatures)
 	ans := make([]int, n)
-	var st []int //此处使用单调栈
+	var st []int
 	for i, t := range temperatures {
-		for len(st) > 0 && t > temperatures[st[len(st)-1]] { // 栈不为空，且单调栈尾小于t，单调栈维护递增值，且元素不一样
+		for len(st) > 0 && t > temperatures[st[len(st)-1]] {
 			j := st[len(st)-1]
 			st = st[:len(st)-1]
-			ans[j] = i - j // 此处更新答案，更高的气温在i - j天后出现
+			ans[j] = i - j
 		}
 		st = append(st, i)
 	}
