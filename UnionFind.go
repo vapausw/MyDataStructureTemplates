f0c630944ee5a1bd2b3edb0b943d3cfdd14afdc7
Your Name <email@example.com>
2023-12-08 17:33:57 +0800

    add

--- UnionFind.go
+++ UnionFind.go
@@ -5,10 +5,10 @@ type UnionFind struct {
 	Groups int   // 连通分量的个数
 }
 
-func NewUnionFind(n int) UnionFind {
-	fa := make([]int, n)
+func NewUnionFind(n int) UnionFind { // 将点从1开始
+	fa := make([]int, n+1)
 	for i := range fa {
-		fa[i] = -1
+		fa[i] = i
 	}
 	return UnionFind{
 		Fa:     fa,
@@ -16,6 +16,7 @@ func NewUnionFind(n int) UnionFind {
 	}
 }
 
+// Find 查找集合的头节点/ 查找该点的父节点
 func (u *UnionFind) Find(x int) int { // 路径压缩寻找父节点
 	if u.Fa[x] != x {
 		u.Fa[x] = u.Find(u.Fa[x])
@@ -23,16 +24,19 @@ func (u *UnionFind) Find(x int) int { // 路径压缩寻找父节点
 	return u.Fa[x]
 }
 
+// Merge 合并两个集合
 func (u *UnionFind) Merge(from, to int) int { // 合并还可采取启发式合并，深度小的合并到深度较大的节点
 	x, y := u.Find(from), u.Find(to)
 	if x == y {
 		return -1
 	}
+
 	u.Fa[x] = y
 	u.Groups--
 	return y
 }
 
+// Same 检查两个点是否在同一个集合
 func (u *UnionFind) Same(x, y int) bool {
 	return u.Find(x) == u.Find(y)
 }
